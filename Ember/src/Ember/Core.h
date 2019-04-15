05b420d2c92183970d6c007688fff57a39f6e248
Khalid Hassan <khalidhsoliman@gmail.com>
2019-04-15 19:22:22 +0200

    Added BIT definition in Core.h

--- Ember/src/Ember/Core.h
+++ Ember/src/Ember/Core.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #ifdef EMBER_PLATFORM_WINDOWS
-	#ifdef EMBER_BUILD_DLL
-		#define EMBER_API __declspec(dllexport)
-	#else
-		#define EMBER_API __declspec(dllimport)
-	#endif // EMBER_BUILD_DLL
+#ifdef EMBER_BUILD_DLL
+#define EMBER_API __declspec(dllexport)
+#else
+#define EMBER_API __declspec(dllimport)
+#endif // EMBER_BUILD_DLL
 #else 
-	#error Ember only support Windows!
+#error Ember only support Windows!
 #endif // EMBER_PLATFORM_WINDOWS
+
+#define BIT(x) (1 << x)
